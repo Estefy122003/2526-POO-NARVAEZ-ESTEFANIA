35da8d96ee4c699e20415a5478ff1ffddfd527d1
Estefy122003 <en.narvaezz@uea.edu.ec>
2026-03-05 15:33:59 -0500

    Delete SEMANA 12/modelos directory

--- SEMANA 12/modelos/usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, nombre, id_usuario):
-        self.nombre = nombre
-        self.id_usuario = id_usuario
-        # REQUISITO: Lista para libros prestados
-        self.libros_prestados = []
-
-    def __str__(self):
-        return f"Usuario: {self.nombre} (ID: {self.id_usuario})"
